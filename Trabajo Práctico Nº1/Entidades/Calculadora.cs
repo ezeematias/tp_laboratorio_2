b37b3f572904022d7a8cbb294d2f0258d5c4d423
ezeematias <eunia69@gmail.com>
2021-04-05 16:56:28 -0300

    Avances en TP

--- Trabajo Práctico Nº1/Entidades/Calculadora.cs
+++ Trabajo Práctico Nº1/Entidades/Calculadora.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    class Calculadora
+    static class Calculadora
     {
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            double resultado = 0;
+
+            return resultado;
+
+        }
+        
+        private static string ValidarOperador(char operador)
+        {
+            string retorno;
+            switch (operador)
+            {
+                case '-':
+                    retorno = "-";
+                    break;
+                case '/':
+                    retorno = "/";
+                    break;
+                case '*':
+                    retorno = "*";
+                    break;
+                default:
+                    retorno = "+";
+                    break;
+            }
+            return retorno;
+        }
+
     }
 }
--- Trabajo Práctico Nº1/Entidades/Numero.cs
+++ Trabajo Práctico Nº1/Entidades/Numero.cs
@@ -6,8 +6,73 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    class Numero
+    public class Numero
     {
+        private double numero;
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string strNumero)
+        {
+
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            
+            return binario;
+        }
+
+        public string DecimalBinario(double numero)
+        {
+            return numero.ToString();
+        }
+        public string DecimalBinario(string numero)
+        {
+            return numero;
+        }
+
+        private bool EsBinario(string binario)
+        {
+            return true;
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.numero - n2.numero;
+        }
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.numero + n2.numero;
+        }
+        public static double operator *(Numero n1, Numero n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            return n1.numero / n2.numero;
+        }
+
+        private static double ValidarNumero (string strNumero)
+        {
+            double retorno = 0;
+            double.TryParse(strNumero, out retorno);        
+            return retorno;
+        }
+
+        public void SetNumero(string strNumero)
+        {
+            this.numero = ValidarNumero(strNumero);
+        }
 
     }
 }
