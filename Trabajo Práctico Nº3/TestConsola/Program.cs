f7f37abc72c5868c03da07226b3b365edfa46307
ezeematias <eunia69@gmai.com>
2021-06-15 18:01:33 -0300

    Corregido - Exception no controlada en consola.

--- Trabajo Práctico Nº3/TestConsola/Program.cs
+++ Trabajo Práctico Nº3/TestConsola/Program.cs
@@ -10,18 +10,36 @@ namespace TestConsola
     class Program
     {
         static void Main(string[] args)
-        {            
-            Stock.ReadDevices();
-            SerialsNumbers.ReadSerialsNumbers();
-            List<Device> listDevices = new List<Device>();
-
-            listDevices.Add(new AccessControl(ECode.CNT_A60_ID, EType.AccessControl, EValidation.Finger));
-            listDevices.Add(new AccessPanel(ECode.CNT_BioPanel_ACC, EType.PanelAccess, 2, EValidation.Card));
-            listDevices.Add(new Attendance(ECode.CNT_E7_CR, EType.Attendance, EValidation.Face));
-
-            foreach (Device item in listDevices)
+        {
+            try
             {
-                Console.WriteLine(item.ToString());
+                // Reading serialized files
+                Stock.ReadComponents();
+                Stock.ReadDevices();
+                SerialsNumbers.ReadSerialsNumbers();
+
+                // Login operator
+                LoginOperators.Login.OperatorLog = new LoginOperators.Operator("Test", "Console", 9999, 9999);
+
+                // Work order selection
+                CoreSystem.SelectedOrder = new InternalOrder(9999, ECode.CNT_BioPanel_ACC, EType.PanelAccess, 3);
+
+                // Device creation using your own creation method
+                CoreSystem.LoadDevices(EType.PanelAccess, EValidation.Finger);
+                CoreSystem.PreviewDevices.Add(CoreSystem.DeviceAssembly);
+
+                // Manual addition of device                
+                CoreSystem.PreviewDevices.Add(new AccessControl(ECode.CNT_A60_ID, EType.AccessControl, EValidation.Finger));
+                CoreSystem.PreviewDevices.Add(new AccessPanel(ECode.CNT_BioPanel_ACC, EType.PanelAccess, 2, EValidation.Finger));
+                CoreSystem.PreviewDevices.Add(new Attendance(ECode.CNT_E7_CR, EType.Attendance, EValidation.Finger));
+                
+                // All data in the list is printed
+                Console.WriteLine(Device.Info(CoreSystem.PreviewDevices));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
             Console.ReadLine();
         }
